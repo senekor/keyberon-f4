0b2884dbb39bcd3583509beb4cca2473e8d6cb12
Remo Senekowitsch <remo@buenzli.dev>
2024-07-09 16:31:27 +0200

    add my layout

--- src/layout.rs
+++ src/layout.rs
@@ -1,4 +1,4 @@
-use keyberon::action::{k, m, Action::*, HoldTapAction, HoldTapConfig};
+use keyberon::action::{k, Action::*, HoldTapAction, HoldTapConfig};
 use keyberon::key_code::KeyCode::*;
 
 type Action = keyberon::action::Action<()>;
@@ -15,57 +15,31 @@ macro_rules! hold_tap {
     };
 }
 
-const CUT: Action = m(&[LShift, Delete].as_slice());
-const COPY: Action = m(&[LCtrl, Insert].as_slice());
-const PASTE: Action = m(&[LShift, Insert].as_slice());
-
-const C_ESC: Action = hold_tap!(k(LCtrl), Escape);
-
-const G_A: Action = hold_tap!(k(LGui), A);
-const M_S: Action = hold_tap!(k(LAlt), S);
-const C_D: Action = hold_tap!(k(LCtrl), D);
-const S_F: Action = hold_tap!(k(LShift), F);
-const S_J: Action = hold_tap!(k(RShift), J);
-const C_K: Action = hold_tap!(k(RCtrl), K);
-const M_L: Action = hold_tap!(k(LAlt), L);
-const G_SC: Action = hold_tap!(k(RGui), SColon);
-
-const G_1: Action = hold_tap!(k(LGui), Kb1);
-const M_2: Action = hold_tap!(k(LAlt), Kb2);
+const A_2: Action = hold_tap!(k(LAlt), Kb2);
 const C_3: Action = hold_tap!(k(LCtrl), Kb3);
 const S_4: Action = hold_tap!(k(LShift), Kb4);
-const S_7: Action = hold_tap!(k(RShift), Kb7);
-const C_8: Action = hold_tap!(k(RCtrl), Kb8);
-const M_9: Action = hold_tap!(k(LAlt), Kb9);
-const G_0: Action = hold_tap!(k(RGui), Kb0);
-
-const STAB: Action = m(&[LShift, Tab].as_slice());
-
-const COLON: Action = m(&[LShift, N].as_slice());
-const EQUAL: Action = m(&[RAlt, G].as_slice());
-const NBSP: Action = m(&[RAlt, LShift, Space].as_slice());
 
 #[rustfmt::skip]
 pub static LAYERS: keyberon::layout::Layers<12, 4, 4, ()> = keyberon::layout::layout! {
-    { //[+· ···+··· ···+··· ···+··· ···+··· ···+···|···+··· ···+··· ···+··· ···+··· ···+··· ···+],
-        [Tab   Q       W       E       R       T       Y       U       I       O       P     '['],
-        [']' {G_A}   {M_S}   {C_D}   {S_F}     G       H     {S_J}   {C_K}   {M_L}  {G_SC}  '\''],
-        [=     Z       X       C       V       B       N       M       ,       .       /    '\\'],
-        [n     n       n       n     Space    (1)     (2)   RShift   RAlt      n       n       n],
-    }{//[+· ···+··· ···+··· ···+··· ···+··· ···+···|···+··· ···+··· ···+··· ···+··· ···+··· ···+],
-        [n Pause CapsLock ScrollLock PScreen {STAB}    n    BSpace  Delete  Insert     n       n],
-        [n   LGui    LAlt   {C_ESC} LShift    Tab      n     Left    Down     Up     Right     n],
-        [n   Undo    {CUT}  {COPY}  {PASTE}    n     Enter   Home   PgDown   PgUp     End      n],
-        [n     n       t       t       t       n      (3)      t       t       t       n       n],
-    }{//[+· ···+··· ···+··· ···+··· ···+··· ···+···|···+··· ···+··· ···+··· ···+··· ···+··· ···+],
-        [~     !       @       #       $       %       ^       &       *      '('     ')'    '_'],
-        ['`' {G_1}   {M_2}   {C_3}   {S_4}     5       6     {S_7}   {C_8}   {M_9}   {G_0}     -],
-        [n     n    {NBSP}  {COLON}    .    {EQUAL}    N    KpPlus  KpMinus KpSlash KpAsterisk n],
-        [n     n       t       t       t      (3)      n       t       t       t       n       n],
-    }{//[+· ···+··· ···+··· ···+··· ···+··· ···+···|···+··· ···+··· ···+··· ···+··· ···+··· ···+],
-        [n    F1      F2      F3      F4      F5      F6      F7      F8      F9      F10      n],
-        [n   LGui    LAlt    LCtrl  LShift    '['     ']'   RShift   RCtrl   LAlt    RGui      n],
-        [n    F11     F12      n      '`'      n       n     '\''    '\\'      -       =       n],
-        [n     n       t  {Custom(())} t       n       n       t       t       t       n       n],
-    } //[+· ···+··· ···+··· ···+··· ···+··· ···+···|···+··· ···+··· ···+··· ···+··· ···+··· ···+],
+    { //[+·· ···+··· ···+··· ···+··· ···+··· ···+···|···+··· ···+··· ···+··· ···+··· ···+··· ···+],
+        [Escape Q       W       F       P       B       J       L       U       Y     '\'' BSpace],
+        [Tab    A       R       S       T       G       M       N       E       I       O   Enter],
+        [LShift Z       X       C       D       V       K       H       ,       .       /    RAlt],
+        [n      n       n     LCtrl   Space    (2)   LShift    (1)    LGui      n       n       n],
+    }{//[+·· ···+··· ···+··· ···+··· ···+··· ···+···|···+··· ···+··· ···+··· ···+··· ···+··· ···+],
+        [t      n       $      '{'     '}'      %       @       |      '`'      #     '\''      t],
+        [t      &       !      '('     ')'     '_'      ^       =      '"'      ;       :       t],
+        [t      +       *      '['     ']'    '\\'      ~       -       <       >       ?       t],
+        [t      t       t       t       t       t       t       t       t       t       t       t],
+    }{//[+·· ···+··· ···+··· ···+··· ···+··· ···+···|···+··· ···+··· ···+··· ···+··· ···+··· ···+],
+        [t      n     Home     Up      End      n       5       6       7       8       9       t],
+        [t    LGui    Left    Down    Right  Delete     0       1     {A_2}   {C_3}   {S_4}     t],
+        [t      n     PgUp      n    PgDown     n      '_'      -       ,       .       /       t],
+        [t      t       t       t       t       t       t       t       t       t       t       t],
+    }{//[+·· ···+··· ···+··· ···+··· ···+··· ···+···|···+··· ···+··· ···+··· ···+··· ···+··· ···+],
+        [F1    F2      F3      F4      F5      F6      F7      F8      F9      F10     F11    F12],
+        [t      n       n       n       n       n       n       n       n       n       n       t],
+        [t      n       n       n       n       n       n       n       n       n       n       t],
+        [t      t       t       t       t       t       t       t       t       t       t       t],
+    } //[+·· ···+··· ···+··· ···+··· ···+··· ···+···|···+··· ···+··· ···+··· ···+··· ···+··· ···+],
 };
